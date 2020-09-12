2e16482430c056f946398db5bbb952e6174279e8
MatsuuraKousei <60458111+HIMAZINX@users.noreply.github.com>
2020-09-12 21:28:30 +0900

    初期project

---
+++ MyProject/BaseFramework/Src/System/KdSystem.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+//====================================================
+//
+// システム系のヘッダーファイル
+//
+//====================================================
+
+// 便利機能
+#include "Utility/KdUtility.h"
+
+// バッファ
+#include "Direct3D/KdBuffer.h"
+// Direct3D
+#include "Direct3D/KdDirect3D.h"
+
+
+// テクスチャ
+#include "Direct3D/KdTexture.h"
+// メッシュ
+#include "Direct3D/KdMesh.h"
+
+
+// シェーダ
+#include "Shader/KdShaderManager.h"
+
+// ウィンドウ
+#include "Window/KdWindow.h"
+
+
